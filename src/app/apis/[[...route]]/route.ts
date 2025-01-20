6b9450134534ba5c21bf9ab284c9be531ee4520a
Valentyn Kasyan <vkasyan@gmx.de>
2025-01-20 23:47:03 +0100

    Hono.js set-up part 3:  endpoints in separate files from route.ts

--- src/app/apis/[[...route]]/route.ts
+++ src/app/apis/[[...route]]/route.ts
@@ -1,28 +1,19 @@
 // import { z } from "zod";
 // import { zValidator } from "@hono/zod-validator";
+// import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import authors from "./authors";
+import books from "./books";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/apis");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      error: "Unauthorized",
-    });
-  }
-
-  return c.json({
-    message: "Hello Next.js!",
-    userId: auth.userId,
-  });
-});
+app.route("/authors", authors);
+app.route("/books", books);
 
 export const GET = handle(app);
 export const POST = handle(app);
