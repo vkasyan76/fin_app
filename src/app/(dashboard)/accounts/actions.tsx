6b18a5c75067c771e864588007a1b4f4279706d9
Valentyn Kasyan <vkasyan@gmx.de>
2025-02-01 21:49:29 +0100

    Accounts Page: Delete account action in Actions Column

--- src/app/(dashboard)/accounts/actions.tsx
+++ src/app/(dashboard)/accounts/actions.tsx
@@ -8,9 +8,13 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
 import { EditAccountSheet } from "./edit-account-sheet";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useMutation } from "convex/react";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { api } from "../../../../convex/_generated/api";
+import { toast } from "sonner";
 
 type Props = {
   id: Id<"accounts">; // Use the account ID type
@@ -18,6 +22,12 @@ type Props = {
 
 export const Actions = ({ id }: Props) => {
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const deleteMutation = useMutation(api.accounts.remove);
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this account."
+  );
 
   const handleEdit = () => {
     setIsEditOpen(true);
@@ -27,8 +37,22 @@ export const Actions = ({ id }: Props) => {
     setIsEditOpen(false);
   };
 
+  const handleDelete = async () => {
+    const confirmed = await confirm();
+    if (confirmed) {
+      try {
+        await deleteMutation({ ids: [id] });
+        toast.success("Account deleted successfully!");
+      } catch (error) {
+        console.error("Error deleting account:", error);
+        toast.error("Failed to delete account.");
+      }
+    }
+  };
+
   return (
     <>
+      <ConfirmDialog />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="size-8 p-0">
@@ -39,6 +63,10 @@ export const Actions = ({ id }: Props) => {
           <DropdownMenuItem onClick={handleEdit}>
             <Edit className="mr-2 h-4 w-4" />
             Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleDelete}>
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
